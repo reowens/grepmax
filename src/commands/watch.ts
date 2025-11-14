0b1f5d599ce851d52a5aecaabc95a901d07b335d
Daniel Wasmus <130555974+danielwsms@users.noreply.github.com>
2025-11-14 21:33:32 +0100

    chore: fix typos (#13)
    * chore: fix typos
    
    * chore: lowercase name

--- src/commands/watch.ts
+++ src/commands/watch.ts
@@ -26,7 +26,7 @@ export const watch = new Command("watch")
           await store.create({
             name: options.store,
             description:
-              "mgrep store - Mixedbreads mulitmodal mulitlingual magic search",
+              "mgrep store - Mixedbreads multimodal multilingual magic search",
           });
         }
         const result = await initialSync(
